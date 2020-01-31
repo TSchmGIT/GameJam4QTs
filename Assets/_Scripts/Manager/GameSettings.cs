396d34464d7f946cf571a0eede069cfbce56d5d8
Thorsten Schmiedel <schmiedel.thorsten@gmail.com>
2020-02-01 00:01:26 +0100

    Added basic game flow (not fully available)
    There are 2 scenes, there is a basic game flow from the menu to the game scene

---
+++ Assets/_Scripts/Manager/GameSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = nameof(GameSettings), menuName = "Game Settings")]
+public class GameSettings : ScriptableObject
+{
+	public float GameTime = 0.0f;
+	public float CountdownTime = 3.0f;
+
+	public bool ShowTutorialScreen = true;
+}
---
+++ Assets/_Scripts/Manager/GameTickManager.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTickManager
+{
+	#region Properties
+	private bool isInCooldown => m_StartCountdownTimer > 0.0f;
+	#endregion
+
+	#region Private Variables
+	public float m_StartCountdownTimer = 0.0f;
+	private float m_GameTimeLeft = 0.0f;
+	private int m_Points = 0;
+
+	private List<GameObject> m_PlayerList = new List<GameObject>();
+	#endregion
+	
+	#region Public Methods
+
+	public void ResetGame()
+	{
+		m_GameTimeLeft = GameManager.Instance.settings.GameTime;
+		m_StartCountdownTimer = GameManager.Instance.settings.CountdownTime;
+	}
+
+	public void Tick()
+	{
+		if (isInCooldown)
+		{
+			bool cooldownHasPassed = UpdateCooldown();
+		}
+		else
+		{
+			bool gameHasEnded = UpdateTime();
+		}
+	}
+
+	private bool UpdateCooldown()
+	{
+		if (m_StartCountdownTimer >= 0.0f)
+		{
+			m_StartCountdownTimer -= Time.deltaTime;
+			return false;
+		}
+		else
+		{
+			m_StartCountdownTimer = 0.0f;
+			return true;
+		}
+	}
+
+	public void AddPoints(int points)
+	{
+		m_Points += points;
+	}
+
+	public void Init()
+	{
+		
+	}
+	#endregion
+
+	#region Private Methods
+	private bool UpdateTime()
+	{
+		// Check if the game time has run out
+		if (m_GameTimeLeft >= 0)
+		{
+			m_GameTimeLeft -= Time.deltaTime;
+			return false;
+		}
+		else
+		{
+			m_GameTimeLeft = 0.0f;
+
+			GameManager.Instance.EndGame();
+			return true;
+		}
+	}
+
+	private void StartGame()
+	{
+		ActivatePlayerList();
+	}
+
+	private void ActivatePlayerList()
+	{
+
+	}
+	#endregion
+}
